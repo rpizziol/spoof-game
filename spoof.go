caa8d9e5a6bcdecfb2da6ad7954a29a90addfb55
rpizziol <roberto.pizziol@gmail.com>
2021-03-09 15:31:24 +0100

    Find winner of single round implemented

--- spoof.go
+++ spoof.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"spoof-game/learning-go/util"
 	"time"
 )
 
@@ -23,59 +24,61 @@ const minCoins = 0
 
 func routineJob(position int, channel []chan []int, guesses []int) {
 	// Initialize
-	myCoins := drawCoins()
-	player := Player{initiator: position == 0, position: position, coins: myCoins}
+	player := Player{initiator: position == 0, position: position, coins: drawCoins()}
 	fmt.Println(player.printPlayer())
 
 	if player.initiator {
-		player.guess = player.guessCoins(myCoins, guesses)
+		player.guess = player.guessCoins(player.coins, guesses)
 		guesses[position] = player.guess
 		// Update the overall value of coins on the table
-		guesses[len(guesses)-1] = myCoins
+		guesses[len(guesses)-1] = player.coins
 		// Pass array of guesses to the next player
 		channel[position] <- guesses
 		//close(channel[position])
 		guesses := <-channel[(position+numberOfPlayers-1)%numberOfPlayers]
 		fmt.Printf("Player %d: %v\n", player.position, guesses)
+		// Guesses round is over
+		var winner = findWinner(guesses)
+		fmt.Printf("Player %d: the winner is Player %d\n", player.position, winner)
 	} else {
 		// Wait for message on the receiving channel
 		guesses := <-channel[(position+numberOfPlayers-1)%numberOfPlayers]
 		fmt.Printf("Player %d: %v received\n", player.position, guesses)
 		// Send guesses array on the sending channel
-		player.guess = player.guessCoins(myCoins, guesses)
+		player.guess = player.guessCoins(player.coins, guesses)
 		guesses[position] = player.guess
 		// Update the overall value of coins on the table
-		guesses[len(guesses)-1] += myCoins
+		guesses[len(guesses)-1] += player.coins
 		channel[position] <- guesses
 	}
+}
 
-	//close(channel[position])
-	// If initiator OR pushed
-	// Guess number of coins in the table (a number not already guessed)
-	// Push next thread
-
-	// If initiator AND pushed
-	// Declare guesses round is over
-
-	// Add
-
-	// Wait until all threads declared
-	// Publish number of coins
-	// Calculate total sum (and distance from my guess)
-
+func findWinner(guesses []int) int {
+	fmt.Printf("guesses: %v\n", guesses)
+	var distance = make([]int, numberOfPlayers)
+	for i := 0; i < numberOfPlayers; i++ {
+		distance[i] = util.Abs(guesses[len(guesses)-1] - guesses[i])
+	}
+	fmt.Printf("distance: %v\n", distance)
+	var winner = 0
+	for i := 0; i < numberOfPlayers; i++ {
+		if distance[i] < distance[winner] {
+			winner = i
+		}
+	}
+	return winner
 }
 
 func main() {
 	fmt.Println("Initializing Spoof Game...")
 	rand.Seed(time.Now().UnixNano())
-	// Initialize array of channels
+	// Array of channels
 	var channel = make([]chan []int, numberOfPlayers)
-	// Initialize each single channel
-	for i := range channel {
+	for i := range channel { // Initialize each single channel
 		channel[i] = make(chan []int)
 	}
 
-	// The element passed between players
+	// The element passed between players (array of guesses + money box)
 	// (NB The last place is reserved to the real value, updated by each player)
 	var guesses = make([]int, numberOfPlayers+1)
 
@@ -83,6 +86,6 @@ func main() {
 	for i := 0; i < numberOfPlayers; i++ {
 		go routineJob(i, channel, guesses)
 	}
+	// TODO fix this with wait group?
 	fmt.Scanln()
-	//fmt.Println(guesses)
 }
